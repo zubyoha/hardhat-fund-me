6b3bc14c329c7ddf11b85875fe1f9feaefd6d6d0
Zuby Oha <zubyoha@outlook.com>
2024-01-12 14:48:06 +0100

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,38 @@
+// function deployFunc(hre) {
+//    console.log("Hi!")
+//}
+// module.exports.default = deployFunc
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("----------------------------------------------------")
+    // well what happens whenwe want to change chains?
+    // when going for localhost or hardhat network we want to use a mock
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log(`FundMe deployed at ${fundMe.address}`)
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+}
+module.exports.tags = ["all", "fundme"]
---
+++ helper-hardhat-config.js
@@ -0,0 +1,16 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+    },
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
